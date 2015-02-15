101cc4578e89cb89e60505e6b3e903f0725d7ad5
avonizos <oa.sozinova@gmail.com>
2015-02-16 01:19:39 +0300

    First version.

---
+++ beserdict_site.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+from lxml import etree
+from flask import Flask, url_for, request, render_template, redirect, session, jsonify
+import codecs, re, uuid
+
+app = Flask(__name__)
+app.secret_key = 'k_bXnlu654Q'
+sessionData = {}    # session key -> dictionary with the data for current session
+dictTree = None
+lemmas = []
+
+def load_dictionary(fname):
+    global dictTree, lemmas
+    dictTree = etree.parse(fname)
+    lemmaEls = dictTree.xpath(u'/root/Lemma')
+    for lemmaEl in lemmaEls:
+        lemmaSignEl = lemmaEl.xpath(u'Lemma.LemmaSign')
+        if len(lemmaSignEl) != 1:
+            continue
+        lemma = lemmaSignEl[0].xpath(u'string()')
+        if u'[' in unicode(lemma):
+            continue
+        homonymNumberEl = lemmaEl.xpath(u'Lemma.HomonymNumber')
+        if len(homonymNumberEl) == 1:
+            lemma += u' (' + homonymNumberEl[0].xpath(u'string()') + u')'
+        # print unicode(lemma)
+        lemmas.append(unicode(lemma))
+
+
+def initialize_session():
+    global sessionData
+    session[u'session_id'] = str(uuid.uuid4())
+    sessionData[session[u'session_id']] = {}
+
+    
+def get_session_data(fieldName):
+    global sessionData
+    try:
+        dictCurData = sessionData[session[u'session_id']]
+        requestedValue = dictCurData[fieldName]
+        return requestedValue
+    except:
+        return None
+
+
+def set_session_data(fieldName, value):
+    global sessionData
+    if u'session_id' not in session:
+        initialize_session()
+    sessionData[session[u'session_id']][fieldName] = value
+
+
+def in_session(fieldName):
+    global sessionData
+    if u'session_id' not in session:
+        return False
+    return fieldName in sessionData[session[u'session_id']]
+
+
+def find_element(lemma):
+    global dictTree
+    homonymNum = 0
+    m = re.search(u'^(.+) \\(([0-9]+)\\)$', lemma, flags=re.U)
+    if m != None:
+        lemma = m.group(1)
+        homonymNum = int(m.group(2))
+    try:
+        if homonymNum <= 0:
+            entryEl = dictTree.xpath(u'/root/Lemma[Lemma.LemmaSign/text()=\'' +
+                                     lemma + u'\']')[0]
+        else:
+            entryEl = dictTree.xpath(u'/root/Lemma[Lemma.LemmaSign/text()=\'' +
+                                     lemma +
+                                     u'\' and Lemma.HomonymNumber/text()=\'' +
+                                     str(homonymNum) + u'\']')[0]
+    except:
+        return None
+    return entryEl
+
+
+@app.route('/')
+def index():
+    global lemmas
+    return render_template(u'index.html', lemmas=lemmas)
+
+
+@app.route('/_get_entry')
+def get_entry():
+    lemma = request.args.get('lemma', u'', type=unicode).replace(u"'", u'')
+    entryEl = find_element(lemma)
+    if entryEl is None:
+        return jsonify(entryHtml=u'nonono')
+    lemmaSignEl = entryEl.xpath(u'Lemma.LemmaSign')
+    lemmaSign = unicode(lemmaSignEl[0].xpath(u'string()'))
+    
+    homonymNumber = None
+    homonymNumberEl = entryEl.xpath(u'Lemma.HomonymNumber')
+    if len(homonymNumberEl) == 1:
+        homonymNumber = unicode(homonymNumberEl[0].xpath(u'string()'))
+
+    lemmaStatus = None
+    lemmaStatusEl = entryEl.xpath(u'Lemma.Status')
+    if len(lemmaStatusEl) == 1:
+        lemmaStatus = unicode(lemmaStatusEl[0].xpath(u'string()'))
+
+
+    psBlocks = []
+    psBlockEls = entryEl.xpath(u'PSBlock')
+    for psBlockEl in psBlockEls:
+        psBlock = {u'psbPS': u'?', u'psbMU': [], u'psNounGram': [], u'idioms': [], u'values': []}
+        psBlocks.append(psBlock)
+        posEl = psBlockEl.xpath(u'PSBlock.PsbPS')
+        if len(posEl) == 1:
+            psBlock[u'psbPS'] = unicode(posEl[0].xpath(u'string()'))
+
+        psBlockNounGrams = psBlockEl.xpath(u'PSNounGram')
+        for psBlockNounGramEl in psBlockNounGrams:
+            psNounGram = {u'oblStem': u''}
+            psNounGramEl = psBlockNounGramEl.xpath(u'PSNounGram.OblStem')
+            if len(psNounGramEl) == 1:
+                psNounGram[u'oblStem'] = unicode(psNounGramEl[0].xpath(u'string()'))
+            psBlock[u'psNounGram'].append(psNounGram)
+
+        psBlockMUEls = psBlockEl.xpath(u'PsbMU')
+        for psBlockMUEl in psBlockMUEls:
+            psbMU = {u'psbMU': u''}
+            psbMUEl = psBlockMUEl.xpath(u'PsbMU.PsbMU')
+            if len(psbMUEl) == 1:
+                psbMU[u'psbMU'] = unicode(psbMUEl[0].xpath(u'string()'))
+            psbMU[u'psbMU'] = re.sub(u'(&lt;|&gt;)', u'', psbMU[u'psbMU'])
+            psBlock[u'psbMU'].append(psbMU)
+
+        idiomEls = psBlockEl.xpath(u'Idiom')
+        for idiomEl in idiomEls:
+            idiom = {u'idiomText': u'', u'idiomTr': u'', u'examples': []}
+            idiomTextEl = idiomEl.xpath(u'Idiom.IdiomText')
+            idiomTrEl = idiomEl.xpath(u'Idiom.IdiomTr')
+            if len(idiomTextEl) == 1:
+                idiom[u'idiomText'] = unicode(idiomTextEl[0].xpath(u'string()'))
+            if len(idiomTrEl) == 1:
+                idiom[u'idiomTr'] = unicode(idiomTrEl[0].xpath(u'string()'))
+            exampleIdiomEls = idiomEl.xpath(u'Example')
+            for exampleEl in exampleIdiomEls:
+                example = {u'exGoesToDict': u'', u'exText': u'', u'exTrans': u''}
+                exGoesToDictEl = exampleEl.xpath(u'Example.ExGoesToDict')
+                exTextEl = exampleEl.xpath(u'Example.ExText')
+                exTransEl = exampleEl.xpath(u'Example.ExTrans')
+                example[u'exGoesToDict'] = unicode(exGoesToDictEl[0].xpath(u'string()'))
+                example[u'exText'] = unicode(exTextEl[0].xpath(u'string()'))
+                example[u'exTrans'] = unicode(exTransEl[0].xpath(u'string()'))
+                idiom[u'examples'].append(example)
+            psBlock[u'idioms'].append(idiom)
+
+        valueEls = psBlockEl.xpath(u'Value')
+        for valueEl in valueEls:
+            value = {u'valNum': u'', u'valTr': u'', u'valTolk': u'', u'examples': []}
+            valNumEl = valueEl.xpath(u'Value.ValNumber')
+            valTrEl = valueEl.xpath(u'Value.ValTr')
+            valTolkEl = valueEl.xpath(u'Value.ValTolk')
+            if len(valTrEl) == 1:
+                value[u'valTr'] = unicode(valTrEl[0].xpath(u'string()'))
+            if len(valTolkEl) == 1:
+                value[u'valTolk'] = unicode(valTolkEl[0].xpath(u'string()'))
+            if len(valNumEl) == 1:
+                value[u'valNum'] = unicode(valNumEl[0].xpath(u'string()'))
+
+            exampleEls = valueEl.xpath(u'Example')
+            for exampleEl in exampleEls:
+                example = {u'exGoesToDict': u'', u'exText': u'', u'exTrans': u''}
+                exGoesToDictEl = exampleEl.xpath(u'Example.ExGoesToDict')
+                exTextEl = exampleEl.xpath(u'Example.ExText')
+                exTransEl = exampleEl.xpath(u'Example.ExTrans')
+                example[u'exGoesToDict'] = unicode(exGoesToDictEl[0].xpath(u'string()'))
+                example[u'exText'] = unicode(exTextEl[0].xpath(u'string()'))
+                example[u'exTrans'] = unicode(exTransEl[0].xpath(u'string()'))
+                value[u'examples'].append(example)
+            psBlock[u'values'].append(value)
+    entry = render_template(u'entry.html', lemmaSign=lemmaSign,
+                            homonymNumber=homonymNumber,
+                            lemmaStatus=lemmaStatus,
+                            psBlocks=psBlocks)
+    
+    return jsonify(entryHtml=entry)
+
+
+def start_server():
+    load_dictionary(u'dict.xml')
+    app.run(host='0.0.0.0', port=2019)
+    #app.config['SERVER_NAME'] = '62.64.12.18:5000'
+   
+if __name__ == u'__main__':
+    start_server()
+
