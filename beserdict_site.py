721c93e0be624e10a73514da34cfe59f540cfabc
avonizos <oa.sozinova@gmail.com>
2015-03-28 01:22:37 +0300

    Added simple search.

--- beserdict_site.py
+++ beserdict_site.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import time
+#import time
 from lxml import etree
 from flask import Flask, url_for, request, render_template, redirect, session, jsonify
-import codecs, re, uuid
+#import codecs
+import re
+import uuid
 
 app = Flask(__name__)
 app.secret_key = 'k_bXnlu654Q'
@@ -12,90 +14,13 @@ sessionData = {}    # session key -> dictionary with the data for current sessio
 dictTree = None
 lemmas = []
 
-def load_dictionary(fname):
-    global dictTree, lemmas
-    dictTree = etree.parse(fname)
-    lemmaEls = dictTree.xpath(u'/root/Lemma')
-    for lemmaEl in lemmaEls:
-        lemmaSignEl = lemmaEl.xpath(u'Lemma.LemmaSign')
-        if len(lemmaSignEl) != 1:
-            continue
-        lemma = lemmaSignEl[0].xpath(u'string()')
-        if u'[' in unicode(lemma):
-            continue
-        homonymNumberEl = lemmaEl.xpath(u'Lemma.HomonymNumber')
-        if len(homonymNumberEl) == 1:
-            lemma += u' (' + homonymNumberEl[0].xpath(u'string()') + u')'
-        # print unicode(lemma)
-        lemmas.append(unicode(lemma))
-
-
-def initialize_session():
-    global sessionData
-    session[u'session_id'] = str(uuid.uuid4())
-    sessionData[session[u'session_id']] = {}
-
-    
-def get_session_data(fieldName):
-    global sessionData
-    try:
-        dictCurData = sessionData[session[u'session_id']]
-        requestedValue = dictCurData[fieldName]
-        return requestedValue
-    except:
-        return None
-
-
-def set_session_data(fieldName, value):
-    global sessionData
-    if u'session_id' not in session:
-        initialize_session()
-    sessionData[session[u'session_id']][fieldName] = value
-
-
-def in_session(fieldName):
-    global sessionData
-    if u'session_id' not in session:
-        return False
-    return fieldName in sessionData[session[u'session_id']]
-
-
-def find_element(lemma):
-    global dictTree
-    homonymNum = 0
-    m = re.search(u'^(.+) \\(([0-9]+)\\)$', lemma, flags=re.U)
-    if m != None:
-        lemma = m.group(1)
-        homonymNum = int(m.group(2))
-    try:
-        if homonymNum <= 0:
-            entryEl = dictTree.xpath(u'/root/Lemma[Lemma.LemmaSign/text()=\'' +
-                                     lemma + u'\']')[0]
-        else:
-            entryEl = dictTree.xpath(u'/root/Lemma[Lemma.LemmaSign/text()=\'' +
-                                     lemma +
-                                     u'\' and Lemma.HomonymNumber/text()=\'' +
-                                     str(homonymNum) + u'\']')[0]
-    except:
-        return None
-    return entryEl
-
-
-@app.route('/')
-def index():
-    global lemmas
-    return render_template(u'index.html', lemmas=lemmas)
-
-
-@app.route('/_get_entry')
-def get_entry():
-    lemma = request.args.get('lemma', u'', type=unicode).replace(u"'", u'')
+def find_entry(lemma):
     entryEl = find_element(lemma)
     if entryEl is None:
         return jsonify(entryHtml=u'nonono')
     lemmaSignEl = entryEl.xpath(u'Lemma.LemmaSign')
     lemmaSign = unicode(lemmaSignEl[0].xpath(u'string()'))
-    
+
     homonymNumber = None
     homonymNumberEl = entryEl.xpath(u'Lemma.HomonymNumber')
     if len(homonymNumberEl) == 1:
@@ -182,8 +107,114 @@ def get_entry():
                             homonymNumber=homonymNumber,
                             lemmaStatus=lemmaStatus,
                             psBlocks=psBlocks)
+    return entry
+
+def load_dictionary(fname):
+    global dictTree, lemmas
+    dictTree = etree.parse(fname)
+    lemmaEls = dictTree.xpath(u'/root/Lemma')
+    for lemmaEl in lemmaEls:
+        lemmaSignEl = lemmaEl.xpath(u'Lemma.LemmaSign')
+        if len(lemmaSignEl) != 1:
+            continue
+        lemma = lemmaSignEl[0].xpath(u'string()')
+        if u'[' in unicode(lemma):
+            continue
+        homonymNumberEl = lemmaEl.xpath(u'Lemma.HomonymNumber')
+        if len(homonymNumberEl) == 1:
+            lemma += u' (' + homonymNumberEl[0].xpath(u'string()') + u')'
+        # print unicode(lemma)
+        lemmas.append(unicode(lemma))
+
+
+def initialize_session():
+    global sessionData
+    session[u'session_id'] = str(uuid.uuid4())
+    sessionData[session[u'session_id']] = {}
+
     
+def get_session_data(fieldName):
+    global sessionData
+    try:
+        dictCurData = sessionData[session[u'session_id']]
+        requestedValue = dictCurData[fieldName]
+        return requestedValue
+    except:
+        return None
+
+
+def set_session_data(fieldName, value):
+    global sessionData
+    if u'session_id' not in session:
+        initialize_session()
+    sessionData[session[u'session_id']][fieldName] = value
+
+
+def in_session(fieldName):
+    global sessionData
+    if u'session_id' not in session:
+        return False
+    return fieldName in sessionData[session[u'session_id']]
+
+
+def find_element(lemma):
+    global dictTree
+    homonymNum = 0
+    m = re.search(u'^(.+) \\(([0-9]+)\\)$', lemma, flags=re.U)
+    if m != None:
+        lemma = m.group(1)
+        homonymNum = int(m.group(2))
+    try:
+        if homonymNum <= 0:
+            entryEl = dictTree.xpath(u'/root/Lemma[Lemma.LemmaSign/text()=\'' +
+                                     lemma + u'\']')[0]
+        else:
+            entryEl = dictTree.xpath(u'/root/Lemma[Lemma.LemmaSign/text()=\'' +
+                                     lemma +
+                                     u'\' and Lemma.HomonymNumber/text()=\'' +
+                                     str(homonymNum) + u'\']')[0]
+    except:
+        return None
+    return entryEl
+
+@app.route('/')
+def index():
+    global lemmas
+    return render_template(u'index.html', lemmas=lemmas)
+
+@app.route('/_get_entry')
+def get_entry():
+    lemma = request.args.get('lemma', u'', type=unicode).replace(u"'", u'')
+    entry = find_entry(lemma)
     return jsonify(entryHtml=entry)
+
+def search_elements(req):
+    global dictTree
+    results = []
+    lemmaEls = dictTree.xpath(u'/root/Lemma')
+    for lemmaEl in lemmaEls:
+        lemmaSignEl = lemmaEl.xpath(u'Lemma.LemmaSign')
+        if len(lemmaSignEl) != 1:
+            continue
+        lemma = lemmaSignEl[0].xpath(u'string()')
+        if req in lemma:
+            results.append(lemma)
+    return results
+
+@app.route('/handler/', methods=['GET'])
+def handler():
+    req = request.args.get('word')
+    results = search_elements(req)
+    print results
+    if len(results) == 1:
+        entry = find_entry(results[0])
+        return jsonify(entryHtml = entry, entryAmount = len(results))
+    else:
+        htmls = ''
+        for result in results:
+            htmlString = '<p><a href="javascript:void();" id="lemma">' + result + '</a></p>'
+            htmls += htmlString
+        return jsonify(entryAmount = len(results), entries = htmls)
 
 
 def start_server():
